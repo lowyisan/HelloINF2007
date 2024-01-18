43351052c771d95e393e6071513fe16cf1e65cd9
yisanlow <yisanlow@gmail.com>
2024-01-18 17:33:56 +0800

    initial commit

---
+++ app/src/main/java/com/example/helloinf2007/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.helloinf2007
+
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.helloinf2007.ui.theme.HelloINF2007Theme
+import android.util.Log
+import androidx.annotation.RequiresApi
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+
+class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("MainActivity", "OnCreate function is created at $formattedDateTime")
+        setContent {
+            HelloINF2007Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background)
+                {
+                    Column {// Prevents overlapping
+                    Greeting("INF2007")
+                    From("Michael")
+                    }
+                }
+            }
+
+
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    val currentDateTime = LocalDateTime.now()
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    val formattedDateTime = currentDateTime.format(formatter)
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Composable
+fun From(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "From $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HelloINF2007Theme {
+        Column{
+            Greeting("INF2007")
+            From("Michael")
+        }
+    }
+}}
